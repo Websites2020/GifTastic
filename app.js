b97004bf676b39baf23363f34cdded5b3f90f1b2
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2018-06-03 16:46:13 -0400

    working app

---
+++ app.js
@@ -0,0 +1,79 @@
+var cars = ["Ford", "Chevrolet", "Chrysler", "Honda", "Toyota", "Mazda", "Subaru", "Nissan",];
+
+// displayMovieInfo function re-renders the HTML to display the appropriate content
+function displayCarInfo() {
+
+    $("#gifs-appear-here").empty();
+
+  var car = $(this).attr("data-name");
+ 
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    car + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function(response) {
+      var results = response.data;
+
+      for (var i = 0; i < results.length; i++) {
+
+        var rating = results[i].rating;
+
+        var imgURL = results[i].images.fixed_height_still.url;
+
+        var image = $("<img>").attr("src", imgURL);
+
+        $(image).addClass("gif")
+
+      $("#gifs-appear-here").append("Rating: " + rating).append(image);
+
+      }
+    });
+}
+
+function renderButtons() {
+
+  $("#buttons-view").empty();
+
+  for (var i = 0; i < cars.length; i++) {
+
+    var a = $("<button>");
+    
+    a.addClass("car-btn");
+    
+    a.attr("data-name", cars[i]);
+    
+    a.text(cars[i]);
+   
+    $("#buttons-view").append(a);
+  }
+}
+
+$("#add-car").on("click", function(event) {
+  event.preventDefault();
+
+  var car = $("#car-input").val().trim();
+
+  cars.push(car);
+
+  renderButtons();
+});
+
+$(document).on("click", ".car-btn", displayCarInfo);
+
+renderButtons();
+
+$('body').on('click', '.gif', function() {
+    var src = $(this).attr("src");
+  if($(this).hasClass('playing')){
+     //stop
+     $(this).attr('src', src.replace(/\.gif/i, "_s.gif"))
+     $(this).removeClass('playing');
+  } else {
+    //play
+    $(this).addClass('playing');
+    $(this).attr('src', src.replace(/\_s.gif/i, ".gif"))
+  }
+});
